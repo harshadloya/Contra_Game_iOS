975de13c1a87417cc5703f8910b230e121397326
Harshad Loya <harshadloya@Harshads-iMac.local>
2017-04-20 21:56:14 -0400

    Ground added for gameplay

--- p07-loya/GameScene.swift
+++ p07-loya/GameScene.swift
@@ -9,77 +9,81 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene
+{
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var screenWidth = CGFloat()
+    var screenHeight = CGFloat()
     
-    override func didMove(to view: SKView) {
+    var gameBase = SKNode()
+    var gameBaseTile = SKShapeNode()
+
+    var noOfTilesInARow = Int()
+    var noOfTilesInAColumn = Int()
+    
+    override func didMove(to view: SKView)
+    {
+        screenWidth = self.frame.size.width
+        screenHeight = self.frame.size.height
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+        noOfTilesInARow = Int(screenWidth) / 30
+        noOfTilesInAColumn = Int(screenHeight) / 30
+        
+        for i in 0...noOfTilesInAColumn - 1
+        {
+            for j in 0...noOfTilesInARow - 1
+            {
+                gameBaseTile = self.createBackgroundTile()
+                gameBaseTile.position.x = gameBaseTile.position.x + CGFloat(j * 30)
+                gameBaseTile.position.y = gameBaseTile.position.y + CGFloat(i * 30)
+                gameBase.addChild(gameBaseTile)
+            }
         }
+        self.addChild(gameBase)
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+    func createBackgroundTile() -> SKShapeNode
+    {
+        let tile = SKShapeNode(rectOf: CGSize(width: 30.0, height: 30.0))
+        tile.fillColor = UIColor.green
+        tile.strokeColor = UIColor.red
+        tile.position = CGPoint(x: 15, y: 15)
+        tile.zPosition = 1
+        return tile
     }
     
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        for touch in touches
+        {
+            let touchLocation = touch.location(in: self)
+            print(touchLocation)
         }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        for touch in touches
+        {
+            let touchLocation = touch.location(in: self)
+            print(touchLocation)
+        }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        for touch in touches
+        {
+            let touchLocation = touch.location(in: self)
+            print(touchLocation)
+        }
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        for touch in touches
+        {
+            let touchLocation = touch.location(in: self)
+            print(touchLocation)
+        }
     }
     
     
--- p07-loya/GameViewController.swift
+++ p07-loya/GameViewController.swift
@@ -16,14 +16,16 @@ class GameViewController: UIViewController {
         super.viewDidLoad()
         
         if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
+            
+            // Load the SKScene from 'GameScene.sswift'
+            //let scene = GameScene (size: view.bounds.size)
+            let scene = GameScene (size: CGSize(width: 750, height: 420))
+            
+            // Set the scale mode to scale to fit the window
+            scene.scaleMode = .aspectFill
                 
-                // Present the scene
-                view.presentScene(scene)
-            }
+            // Present the scene
+            view.presentScene(scene)
             
             view.ignoresSiblingOrder = true
             
